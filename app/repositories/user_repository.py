e0aed6968502cd2e70cff5b4e8775187abf9e994
yshouishida <yshoudaisihda006@gmail.com>
2026-09-10 18:45:00 +0800

    User update and delete added in repository

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -63,11 +63,100 @@ def get_by_id_repo(id):
 # ADD USER
 #=======================================
 
+def create_user_repo(first_name, last_name, email, password):
+    conn = None
+
+    try:
+        conn = get_connection()
+
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                INSERT INTO tblUser
+                    (first_name, last_name, email, password)
+                VALUES
+                    (%s, %s, %s, %s)
+                """,
+                (first_name, last_name, email, password)
+            )
+            if cursor.rowcount != 1:
+                conn.rollback()
+                return False
+            
+        conn.commit()
+        return True
+    except Exception as e:
+        if conn: conn.rollback()
+        print(f"Error: {e}")
+        return False
+    finally:
+        if conn: conn.close()
+        
+
 #======================================= 
 # UPDATE USER
 #=======================================
 
+def update_user_repo(id, first_name, last_name, email, password):
+    conn = None
+
+    try:
+        conn = get_connection()
+
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT id FROM tblUser WHERE id = %s
+                """,
+                (id,)
+            )
+            if cursor.fetchone() is None:
+                conn.rollback()
+                return False
+
+            cursor.execute(
+                """
+                UPDATE tblUser
+                SET
+                    first_name = %s,
+                    last_name  = %s,
+                    email      = %s,
+                    password   = %s
+                WHERE id       = %s
+                """,
+                (first_name, last_name, email, password, id)
+            )
+
+        conn.commit()
+        return True
+    except Exception as e:
+        if conn: conn.rollback()
+        return False
+    finally:
+        if conn: conn.close()
+
 #======================================= 
 # DELETE USER
 #=======================================
+def delete_user_repo(id):
+    conn = None
+
+    try:
+        conn = get_connection()
+
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                DELETE FROM tblUser WHERE id = %s
+                """,
+                (id,)
+            )
+        conn.commit()
+        return True
+        
+    except Exception as e:
+        if conn: conn.rollback()
+        print(f"Error: {e}")
+    finally:
+        if conn: conn.close()
 
